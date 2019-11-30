1a86d06ab64c6a7c1ec4fdf335f6c182a8da6738
Evie Skinner <evie.skinner@gwle.onmicrosoft.com>
2019-11-30 21:27:01 +0000

    Started adding to boardState enum and GetStateOfBoard()

---
+++ BoardState.cs
@@ -0,0 +1,7 @@
+﻿namespace NoughtsAndCrosses
+{
+    public class BoardState
+    {
+
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace NoughtsAndCrosses
+{
+    class Program
+    {
+        private enum boardState
+        {
+            NOUGHTS_WIN, CROSSES_WIN, DRAW, NOBODY_HAS_WON_YET
+        }
+
+        private static BoardState GetStateOfBoard(string board)
+        {
+
+        }
+
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
