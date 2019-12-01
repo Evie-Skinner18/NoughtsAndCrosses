2a43e1aa0992ae5c4006e8ee49ee4841b54b9708
Evie Skinner <evie.skinner@gwle.onmicrosoft.com>
2019-12-01 13:27:02 +0000

    Added validator class with two methods

--- NoughtsAndCrosses/Validator.cs
+++ NoughtsAndCrosses/Validator.cs
@@ -1,10 +1,33 @@
-﻿namespace NoughtsAndCrosses
+﻿using System.Linq;
+using System.Collections.Generic;
+
+namespace NoughtsAndCrosses
 {
     public class Validator
     {
+        private string _userInput;
+
         public Validator(string userInput)
         {
-
+            _userInput = userInput;
         }
+
+        public string ValidateUserInput()
+        {
+            var message = "";
+            var userInputList = new List<char>();
+            userInputList = _userInput.ToList();
+            var inputHasInvalidCharacter = false;
+
+            foreach (var c in userInputList)
+            {
+                inputHasInvalidCharacter = IsInvalidCharacter(c) ? true : false;
+            }
+
+            return message = inputHasInvalidCharacter ? 
+                "Sorry but your game board is invalid!" : "Ok thanks for that lovely game board!";
+        }
+
+        public bool IsInvalidCharacter(char c) => !(c.Equals('_') || c.Equals('X') || c.Equals('O'));
     }
 }
