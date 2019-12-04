03fb38664261d8cdf736b032f2e50bb46cbfbc22
Evie Skinner <evie.skinner@gwle.onmicrosoft.com>
2019-12-04 15:10:24 +0000

    Removed obsolete classes and added new tests for GetStateOfBoard(). This code is not DRY at all but looks like I'm not allowed to put GetStateOfBoard() in a separate object.

--- NoughtsAndCrosses.Tests/BoardStateTests.cs
+++ NoughtsAndCrosses.Tests/BoardStateTests.cs
@@ -1,5 +1,5 @@
 ﻿using NUnit.Framework;
-using NoughtsAndCrosses;
+using NoughtsAndCrosses.Utilities;
 
 namespace Tests
 {
@@ -10,17 +10,58 @@ namespace Tests
             NOUGHTS_WIN, CROSSES_WIN, DRAW, NOBODY_HAS_WON_YET, BOARD_IS_INVALID
         }
 
-        [SetUp]
-        public void Setup()
+        private static BoardState GetStateOfBoard(string userBoard)
         {
-            
-        }
+            var boardChecker = new BoardChecker(userBoard);
+            var validator = new Validator(userBoard);
+            var validationBoardStateIndex = validator.ValidateUserInput();
+
+            // if board is valid, then we can check for draw, wins etc
+            if (validationBoardStateIndex.Equals(5))
+            {
+                if (boardChecker.IsADraw())
+                {
+                    return BoardState.DRAW;
+                }
+                else
+                {
+                    var winnerMessage = boardChecker.CheckForAWinner();
+                    var boardStateIndex = boardChecker.GetIndexOfWinner(winnerMessage);
+                    return (BoardState)boardStateIndex;
+                }
+            }
+            else
+            {
+                return (BoardState)validationBoardStateIndex;
+            }
+        }      
 
 
         [Test]
         public void CanGetBoardState_ShouldReturn_NOUGHTS_WIN()
         {
+            var noughtsMessage = GetStateOfBoard("XX_OOOX__");
 
+            Assert.That(noughtsMessage, Is.Not.Null);
+            Assert.AreEqual(noughtsMessage, BoardState.NOUGHTS_WIN);
+        }
+
+        [Test]
+        public void CanGetBoardState_ShouldReturn_CROSSES_WIN()
+        {
+            var noughtsMessage = GetStateOfBoard("XXXOO____");
+
+            Assert.That(noughtsMessage, Is.Not.Null);
+            Assert.AreEqual(noughtsMessage, BoardState.CROSSES_WIN);
+        }
+
+        [Test]
+        public void CanGetBoardState_ShouldReturn_NOUGHTS_WIN()
+        {
+            var noughtsMessage = GetStateOfBoard("XX_OOOX__");
+
+            Assert.That(noughtsMessage, Is.Not.Null);
+            Assert.AreEqual(noughtsMessage, BoardState.NOUGHTS_WIN);
         }
     }
 }
